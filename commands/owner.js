09ff7391754cfec5893ef4693bd13c477fef75b6
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-04-09 23:30:25 +0300

    Delete commands directory

--- commands/owner.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "owner",
-    execute: async (sock, msg) => {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text: "Owner: Jimmy 🔥"
-        })
-    }
-}
