af9f2714ecdeaa7ff898e5f48b132fdefef85b24
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-04-24 22:46:28 +0300

    Delete api/receive-sms.js

--- api/receive-sms.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
-export default async function handler(req, res) {
-    const { message } = req.body;
-    const txnId = message.split(" ")[0].toUpperCase();
-    await supabase.from('payments').insert([{ txn_id: txnId, status: 'unused' }]);
-    return res.status(200).send("Poa");
-}
